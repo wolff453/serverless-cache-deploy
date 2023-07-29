dd7a9823f3cbf0d5c0c60b0875e5bf10d39d4ac9
inf00248 <george8234@hotmail.com>
2023-07-29 12:58:47 -0300

    fix: delete all hash and date objects

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { LambdaClient, UpdateFunctionCodeCommand, UpdateFunctionConfigurationComm
 const { join } = require('path')
 
 class MyPlugin {
-  constructor (serverless) {
+  constructor(serverless) {
     this.serverless = serverless
     this.client = new LambdaClient({ region: this.serverless.configurationInput.provider.region })
     this.hooks = {
@@ -14,7 +14,7 @@ class MyPlugin {
     }
   }
 
-  async deploy (configuration, stream) {
+  async deploy(configuration, stream) {
     return Promise.all(Object.keys(configuration.functions).map(async (key) => {
       console.log(`${configuration.service}-${key}`)
       await this.client.send(new UpdateFunctionCodeCommand({
@@ -24,11 +24,11 @@ class MyPlugin {
     }))
   }
 
-  async getZipStream (config) {
+  async getZipStream(config) {
     return fs.promises.readFile(`${process.cwd()}/.serverless/${config.service}.zip`)
   }
 
-  async validateCache (hash, relativeUrl) {
+  async validateCache(hash, relativeUrl) {
     try {
       const dir = await fs.promises.readdir(join(process.cwd(), relativeUrl))
       return dir.some(files => files.includes(hash))
@@ -37,15 +37,15 @@ class MyPlugin {
     }
   }
 
-  getConfiguration () {
+  getConfiguration() {
     return this.serverless.configurationInput
   }
 
-  generateHash (config) {
+  generateHash(config) {
     return crypto.createHash('md5').update(config).digest('hex')
   }
 
-  async createFileCache (filename, relativeUrl) {
+  async createFileCache(filename, relativeUrl) {
     return fs.outputFile(`${join(process.cwd(), relativeUrl)}/${filename}`, filename)
   }
 
@@ -60,22 +60,29 @@ class MyPlugin {
     })
   }
 
-  deleteDateObjects () {
-    const copy = JSON.parse(JSON.stringify(this.getConfiguration()))
-    Object.keys(copy.package).forEach(key => {
-      if (key.includes('artifact')) {
-        Reflect.deleteProperty(copy.package, key)
-      }
+  removeVersionLogicalId(config) {
+    Object.values(config.functions).forEach((value) => {
+      delete value.versionLogicalId
     })
-    Object.entries(copy.provider.compiledCloudFormationTemplate.Resources).forEach(([key, value]) => {
-      if (value?.Type?.includes('AWS::Lambda::Function')) {
-        delete copy.provider.compiledCloudFormationTemplate.Resources[key].Properties.Code
-      }
-    })
-    return JSON.stringify(copy)
   }
 
-  async updateFunctionsConfiguration (layer) {
+  removeArtifactDirectoryName(config) {
+    delete config.package.artifactDirectoryName
+  }
+
+  removeCompiledCloudFormationTemplate(config) {
+    delete config.provider.compiledCloudFormationTemplate
+  }
+
+  deleteDynamicObjects() {
+    const copyConfig = JSON.parse(JSON.stringify(this.getConfiguration()))
+    this.removeVersionLogicalId(copyConfig)
+    this.removeArtifactDirectoryName(copyConfig)
+    this.removeCompiledCloudFormationTemplate(copyConfig)
+    return JSON.stringify(copyConfig)
+  }
+
+  async updateFunctionsConfiguration(layer) {
     const functions = this.getFunctions()
     await Promise.all(Object.entries(functions).map(async functionName => {
       await this.client.send(new UpdateFunctionConfigurationCommand({
@@ -85,15 +92,15 @@ class MyPlugin {
     }))
   }
 
-  getRelativeDir () {
+  getRelativeDir() {
     return this.serverless.configurationInput.custom?.cache?.relativeUrl
   }
 
-  async cache () {
+  async cache() {
     try {
       const config = this.getConfiguration()
       const relativeUrl = this.getRelativeDir()
-      const obj = this.deleteDateObjects()
+      const obj = this.deleteDynamicObjects()
       const hash = this.generateHash(obj)
       const cache = await this.validateCache(hash, relativeUrl)
       console.log('Generated hash:', hash)
